44098a8cfae4c1573174f498761b5591f0ce95d5
Dmitry Nemkov <xeqlol@gmail.com>
2017-09-28 21:06:16 +0500

    bot command attribute

---
+++ Letmebeyourbot/Bot new/BotCommandAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letmebeyourbot.Bot_new
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class BotCommandAttribute : Attribute
+    {
+        public string[] Commands { get; set; }
+        public AccessLevel AccessRequired { get; set; }
+
+        public BotCommandAttribute(AccessLevel accessRequired, params string[] commands)
+        {
+            Commands = commands;
+            AccessRequired = accessRequired;
+        }
+    }
+}
